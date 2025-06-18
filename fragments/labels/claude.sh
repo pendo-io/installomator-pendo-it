71a0d7d3afdec2b986fbfc2faf81d1112b4a08fc
Jared Y <96198719+mobofixer@users.noreply.github.com>
2025-06-18 08:10:47 -0400

    Delete fragments/labels/claude.sh

--- fragments/labels/claude.sh
+++
@@ -1,8 +0,0 @@
-claude)
-	name="Claude"
-	type="zip"
-	downloadINFO="$( getJSONValue "$( curl -s https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/update_manifest.json )" "releases[0].updateTo" )"
-	downloadURL="$( getJSONValue "$downloadINFO" "url" )"
-	appNewVersion="$( getJSONValue "$downloadINFO" "version" )"
-	expectedTeamID="Q6L2SF6YDW"
-	;;
