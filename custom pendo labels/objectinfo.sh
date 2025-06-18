f3adeeaaed53979dfc18074cd1c63f277801f74a
Jared Y <96198719+mobofixer@users.noreply.github.com>
2025-06-18 11:46:52 -0400

    Create objectinfo.sh custom label

---
+++ custom pendo labels/objectinfo.sh
@@ -0,0 +1,7 @@
+objectinfo)
+    name="Object Info"
+    type="zip"
+    downloadURL="$(downloadURLFromGit BIG-RAT Object-Info)"
+    appNewVersion="$(versionFromGit BIG-RAT Object-Info)"
+    expectedTeamID="PS2F6S478M"
+    ;;
