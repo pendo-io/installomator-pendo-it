ce2aa0ebe5406ae053b72a9072f366286ecdae79
Jared Y <96198719+mobofixer@users.noreply.github.com>
2025-07-01 01:01:03 -0400

    Update windsurf.sh

--- custom pendo labels/windsurf.sh
+++ custom pendo labels/windsurf.sh
@@ -1,11 +1,11 @@
 windsurf)
     name="Windsurf"
     type="dmg"
-    appNewVersion="1.10.5"
-    if [[ $(arch) == arm64 ]]; then
-        downloadURL="https://windsurf-stable.codeiumdata.com/darwin-arm64-dmg/stable/ff497a1ec3dde399fde9c001a3e69a58f2739dac/Windsurf-darwin-arm64-1.10.5.dmg"
-    elif [[ $(arch) == i386 ]]; then
-        downloadURL="https://windsurf-stable.codeiumdata.com/darwin-x64-dmg/stable/ff497a1ec3dde399fde9c001a3e69a58f2739dac/Windsurf-darwin-x64-1.10.5.dmg"
+    myARCH="$(/usr/bin/arch)"
+    if [ "$myARCH" != "arm64" ]; then
+        myARCH=x64
     fi
+    downloadURL="$( curl -s https://windsurf.com/editor/releases | tr '"\\' "\n" | grep -m1 "darwin-$myARCH" )"
+    appNewVersion="$( echo "$downloadURL" | awk -F '-' '{ print $NF }' | cut -d '.' -f 1-3 )"
     expectedTeamID="83Z2LHX6XW"
     ;;
