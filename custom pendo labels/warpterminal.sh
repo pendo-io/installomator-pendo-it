7a984e36e93936ad62de8a2f92277fa99c29d99c
Jared Y <96198719+mobofixer@users.noreply.github.com>
2025-07-17 11:23:49 -0400

    Delete custom pendo labels/warpterminal.sh
    removing this label as it is now included in the main script

--- custom pendo labels/warpterminal.sh
+++
@@ -1,6 +0,0 @@
-warpterminal)
-    name="Warp"
-    type="dmg"
-    downloadURL=$(curl -fs 'https://app.warp.dev/download?package=dmg' | grep -oE 'https://[^"]*')
-    expectedTeamID="2BBY89MBSN"
-    ;;
