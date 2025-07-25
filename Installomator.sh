d2249a411747fc66217a66d62bf47c40c26f0849
Jared Y <96198719+mobofixer@users.noreply.github.com>
2025-07-25 08:19:21 -0400

    Update Installomator.sh
    added diabrowser label to script

--- Installomator.sh
+++ Installomator.sh
@@ -3801,6 +3801,13 @@ devonthink)
     appNewVersion=$( echo ${downloadURL} | tr '/' '\n' | grep "[0-9]" | grep "[.]" | head -1 )
     expectedTeamID="679S2QUWR8"
     ;;
+diabrowser)
+	name="Dia"
+	type="dmg"
+	downloadURL="https://releases.diabrowser.com/release/Dia-latest.dmg"
+	appNewVersion="$(curl -fsIL https://releases.diabrowser.com/release/Dia-latest.dmg | grep -i ^location | sed -E 's/.*-([0-9]+\.[0-9]+\.[0-9]+-[0-9]+).*/\1/')"
+	expectedTeamID="S6N382Y83G"
+	;;
 dialog|\
 swiftdialog)
     name="Dialog"
